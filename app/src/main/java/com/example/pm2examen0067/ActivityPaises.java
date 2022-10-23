f22bd1fa682e877f994ad4e451f095c99912a7f5
Josue Martinez <martinezjosue4448@gmail.com>
2022-10-22 23:28:47 -0600

    Inicio de app 23/10/2022

---
+++ app/src/main/java/com/example/pm2examen0067/ActivityPaises.java
@@ -0,0 +1,14 @@
+package com.example.pm2examen0067;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPaises extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paises);
+    }
+}
